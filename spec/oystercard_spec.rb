b4744ece14574f0bbc84b6a86fe869056d7b73ae
Kynosaur <kye.bracey@gmail.com>
2017-05-16 15:21:32 +0100

    Add tests for @journeys, and that touching out creates one journey. Refactor some previous tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,20 +5,23 @@ describe Oystercard do
       expect(subject.balance).to eq(0)
     end
 
+    it 'has an empty list of journeys by default' do
+      expect(subject.journeys).to eq []
+    end
+
     describe '#top_up' do
       it 'can top up the balance' do
         expect{ subject.top_up 10 }.to change{ subject.balance }.by 10
       end
 
       it 'raises and error if top_up exceeds the maximum monetary balance' do
-        max_balance = Oystercard::BALANCE_LIMIT
-        subject.top_up(max_balance)
-        expect{ subject.top_up 1 }.to raise_error "Maximum balance of £#{max_balance} exceeded"
+        subject.top_up(Oystercard::BALANCE_LIMIT)
+        expect{ subject.top_up 1 }.to raise_error "Maximum balance of £#{Oystercard::BALANCE_LIMIT} exceeded"
       end
     end
 
     describe '#in_journey?' do
-      it 'the oystercard should initially not be in use for ajourney' do
+      it 'the oystercard should initially not be in use for a journey' do
         expect(subject).not_to be_in_journey
       end
     end
@@ -30,7 +33,6 @@ describe Oystercard do
         expect(subject).to be_in_journey
       end
       it 'will not touch in if blance it below minimum' do
-        min_balance = Oystercard::MINIMUM_BALANCE
         expect{ subject.touch_in(:entry_station) }.to raise_error "Insufficient balance to travel"
       end
       it 'remembers the entry station' do
@@ -42,11 +44,19 @@ describe Oystercard do
 
     describe '#touch_out' do
       it 'allows a user to touch out' do
-        subject.touch_out
         expect(subject).not_to be_in_journey
       end
       it 'deducts the fare from the balance of the oystercard' do
         expect{ subject.touch_out }.to change{ subject.balance }.by (-Oystercard::MINIMUM_FARE)
+      end
+    end
+
+    describe '#journeys' do
+      it 'touching in and out should create one journey' do
+        subject.top_up(Oystercard::BALANCE_LIMIT)
+        subject.touch_in(:Paddington)
+        subject.touch_out(:Oxford_Circus)
+        expect(subject.journeys).to eq [{entry_station: :Paddington, exit_station: :Oxford_Circus}]
       end
     end
 end
